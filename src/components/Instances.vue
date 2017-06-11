d153a5ac9fe8daf0bf7cafdc751d0414144d1484
Andreas Schaeffer <hanack@nooblounge.net>
2017-06-11 03:25:18 +0200

    Create new instances + added fontawesome icon framework

--- src/components/Instances.vue
+++ src/components/Instances.vue
@@ -12,10 +12,25 @@
     </div>
 
     <b-table striped hover :items="instances" :fields="fields" :current-page="currentPage" :per-page="perPage" :filter="filter">
+      <template slot="port" scope="item">
+        <strong>{{item.item.port}}</strong>
+      </template>
       <template slot="name" scope="item">
         <strong>{{item.item.name}}</strong>
         <br />
         {{item.item.description}}
+      </template>
+      <template slot="autostart" scope="item">
+        <icon name="check-square-o" v-if="item.item.autostart"></icon>
+        <icon name="square-o" v-if="!item.item.autostart"></icon>
+      </template>
+      <template slot="autoconnect" scope="item">
+        <icon name="check-square-o" v-if="item.item.autoconnect"></icon>
+        <icon name="square-o" v-if="!item.item.autoconnect"></icon>
+      </template>
+      <template slot="autorestart" scope="item">
+        <icon name="check-square-o" v-if="item.item.autorestart"></icon>
+        <icon name="square-o" v-if="!item.item.autorestart"></icon>
       </template>
       <template slot="actions" scope="item">
         <b-btn v-if="item.item.state == 'stopped'" size="sm" @click="changeStateAction(item.item, 'start')">Start</b-btn>
@@ -28,6 +43,31 @@
     </b-table>
     <div class="justify-content-center row my-1">
       <b-pagination size="md" :total-rows="this.instances.length" :per-page="perPage" v-model="currentPage" />
+    </div>
+    <br />
+    <div class="justify-content-center row my-1">
+      <b-form-fieldset horizontal label="Id" class="col-4" :label-size="1">
+        <b-form-input v-model="id" placeholder="Id"></b-form-input>
+      </b-form-fieldset>
+      <b-form-fieldset horizontal label="Type" class="col-4" :label-size="1">
+        <b-form-input v-model="type" placeholder="Type"></b-form-input>
+      </b-form-fieldset>
+      <b-form-fieldset horizontal label="Name" class="col-4" :label-size="1">
+        <b-form-input v-model="name" placeholder="Name"></b-form-input>
+      </b-form-fieldset>
+    </div>
+    <div class="justify-content-center row my-1">
+      <b-form-fieldset horizontal label="Description" class="col-4" :label-size="1">
+        <b-form-input v-model="description" placeholder="Description"></b-form-input>
+      </b-form-fieldset>
+      <b-form-fieldset horizontal label="Autostart" class="col-4" :label-size="1">
+        <b-form-checkbox v-model="autostart"></b-form-checkbox>
+        <b-form-checkbox v-model="autoconnect"></b-form-checkbox>
+        <b-form-checkbox v-model="autorestart"></b-form-checkbox>
+      </b-form-fieldset>
+      <b-form-fieldset horizontal class="col-4">
+        <b-btn @click="createInstance(id, type, name, description, autostart, autoconnect, autorestart)">Create new instance</b-btn>
+      </b-form-fieldset>
     </div>
   </div>
 </template>
@@ -72,17 +112,32 @@ export default {
           this.getInstances();
         });
     },
+    createInstance(id, type, name, description, autostart, autoconnect, autorestart) {
+      const url = `http://localhost:31416/api/v1/instances/${id}`;
+      axios.post(url, {
+        type,
+        name,
+        description,
+        persistent: false,
+        autostart,
+        autoconnect,
+        autorestart,
+      })
+        .then(() => {
+          this.getInstances();
+        });
+    },
   },
   data: () => ({
     instances: [],
     errors: [],
     fields: {
-      name: {
-        label: 'Name',
-        sortable: true,
-      },
       port: {
         label: 'Port',
+        sortable: true,
+      },
+      name: {
+        label: 'Name',
         sortable: true,
       },
       state: {
@@ -112,6 +167,13 @@ export default {
     currentPage: 1,
     perPage: 5,
     filter: null,
+    id: '',
+    type: 'server',
+    name: '',
+    description: '',
+    autostart: true,
+    autoconnect: true,
+    autorestart: true,
   }),
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -2,16 +2,20 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
-// import BootstrapVue from 'bootstrap-vue';
+import 'vue-awesome/icons';
 
 // Import the styles directly. (Or you could add them via script tags.)
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
+import Icon from 'vue-awesome/components/Icon';
+
 import App from './App';
 import router from './router';
 
 Vue.use(BootstrapVue);
+
+Vue.component('icon', Icon);
 
 Vue.config.productionTip = false;
 
